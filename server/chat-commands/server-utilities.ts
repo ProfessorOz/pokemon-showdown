d8368bd2c86c077638df41bbcbcfb2832bfa34d1
Alex Jr <59788045+ProfessorOz@users.noreply.github.com>
2020-01-15 09:56:32 +0530

    Create server-utilities.ts

---
+++ server/chat-commands/server-utilities.ts
@@ -0,0 +1,142 @@
+"use strict";
+
+import { FS } from "../../lib/fs";
+const https = require("https");
+const Autolinker = require("autolinker");
+
+const regdateCache = {};
+
+exports.Server = {
+  // usage: Server.nameColor(user.name, true) for bold OR Server.nameColor(user.name, false) for non-bolded.
+  nameColor: function(name, bold, userGroup) {
+    let userGroupSymbol = Users.usergroups[toId(name)]
+      ? "<b><font color=#948A88>" +
+        Users.usergroups[toId(name)].substr(0, 1) +
+        "</font></b>"
+      : "";
+    return (
+      (userGroup ? userGroupSymbol : "") +
+      (bold ? "<b>" : "") +
+      "<font color=" +
+      Server.hashColor(name) +
+      ">" +
+      (Users(name) && Users(name).connected && Users.getExact(name)
+        ? Chat.escapeHTML(Users.getExact(name).name)
+        : Chat.escapeHTML(name)) +
+      "</font>" +
+      (bold ? "</b>" : "")
+    );
+  },
+
+  regdate: function(target, callback) {
+    target = toId(target);
+    if (regdateCache[target]) return callback(regdateCache[target]);
+    let req = https.get(
+      "https://pokemonshowdown.com/users/" + target + ".json",
+      res => {
+        let data = "";
+        res
+          .on("data", chunk => {
+            data += chunk;
+          })
+          .on("end", () => {
+            try {
+              data = JSON.parse(data);
+            } catch (e) {
+              return callback(false);
+            }
+            let date = data["registertime"];
+            if (date !== 0 && date.toString().length < 13) {
+              while (date.toString().length < 13) {
+                date = Number(date.toString() + "0");
+              }
+            }
+            if (date !== 0) {
+              regdateCache[target] = date;
+              saveRegdateCache();
+            }
+            callback(date === 0 ? false : date);
+          });
+      }
+    );
+    req.end();
+  },
+
+  reloadCSS: function() {
+    const cssPath = "impulse"; // This should be the server id if Config.serverid doesn't exist. Ex: 'serverid'
+    let req = https.get(
+      "https://play.pokemonshowdown.com/customcss.php?server=" +
+        (Config.serverid || cssPath),
+      () => {}
+    );
+    req.end();
+  },
+
+  parseMessage: function(message) {
+    if (message.substr(0, 5) === "/html") {
+      message = message.substr(5);
+      message = message.replace(
+        /\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g,
+        "<i>$1</i>"
+      ); // italics
+      message = message.replace(
+        /\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g,
+        "<b>$1</b>"
+      ); // bold
+      message = message.replace(
+        /\~\~([^< ](?:[^<]*?[^< ])?)\~\~/g,
+        "<strike>$1</strike>"
+      ); // strikethrough
+      message = message.replace(
+        /&lt;&lt;([a-z0-9-]+)&gt;&gt;/g,
+        '&laquo;<a href="/$1" target="_blank">$1</a>&raquo;'
+      ); // <<roomid>>
+      message = Autolinker.link(message.replace(/&#x2f;/g, "/"), {
+        stripPrefix: false,
+        phone: false,
+        twitter: false
+      });
+      return message;
+    }
+    message = Chat.escapeHTML(message).replace(/&#x2f;/g, "/");
+    message = message.replace(
+      /\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g,
+      "<i>$1</i>"
+    ); // italics
+    message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, "<b>$1</b>"); // bold
+    message = message.replace(
+      /\~\~([^< ](?:[^<]*?[^< ])?)\~\~/g,
+      "<strike>$1</strike>"
+    ); // strikethrough
+    message = message.replace(
+      /&lt;&lt;([a-z0-9-]+)&gt;&gt;/g,
+      '&laquo;<a href="/$1" target="_blank">$1</a>&raquo;'
+    ); // <<roomid>>
+    message = Autolinker.link(message, {
+      stripPrefix: false,
+      phone: false,
+      twitter: false
+    });
+    return message;
+  },
+
+  randomString: function(length) {
+    return Math.round(
+      Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)
+    )
+      .toString(36)
+      .slice(1);
+  }
+};
+
+// last two functions needed to make sure Server.regdate() fully works
+function loadRegdateCache() {
+  try {
+    regdateCache = JSON.parse(FS("database/regdate.json").readIfExistsSync());
+  } catch (e) {}
+}
+loadRegdateCache();
+
+function saveRegdateCache() {
+  FS("database/regdate.json").writeSync(JSON.stringify(regdateCache));
+}
